898fa05969ef0dea7ad7423ecfe6d2a5703f16a4
012-user <moshe@gmail.com>
2015-01-01 12:15:46 +0200

    add support to heroku

--- app.js
+++ app.js
@@ -147,13 +147,13 @@ app.post('/WhatsOut', function(request, response)
 }
 );
 
-mongo.connect('mongodb://localhost/app', function(err, aDb) {
+mongo.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/app', function(err, aDb) {
   if (err) {
     throw err;
   }
 
   db = aDb;
-  var server = app.listen(3000, function() {
+  var server = app.listen(process.env.PORT || 3000, function() {
     var host = server.address().address;
     var port = server.address().port;
 
